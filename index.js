d82893299c63ae26b81aa7b470e3b844a4df76df
Om Rathod <130174092+OkRathod@users.noreply.github.com>
2025-04-16 10:21:58 +0530

    Update index.js
    Data added

--- index.js
+++ index.js
@@ -1,11 +1,288 @@
-let quizData ={}
-        fetch('./data.json')
-        .then(response => response.json())
-        .then(data => {
-            quizData = data
-            console.log(data)
-        });
-
+let quizData ={
+    "forest": {
+      "week1": [
+        {
+          "question": "Which of these is not a consumptive value?",
+          "options": ["timber", "firewood", "non-timber forest products", "education"],
+          "answer": 3
+        },
+        {
+          "question": "Which of these is not a forest type found in India?",
+          "options": ["Mediterranean dry", "Tropical dry", "Montane temperate", "Alpine"],
+          "answer": 0
+        },
+        {
+          "question": "“Plant community, predominantly comprised of trees and other woody vegetation, usually with a closed canopy\" is",
+          "options": ["silvicultural definition of forests", "FAO definition of forests", "legal definition of forests", "ecological definition of forests"],
+          "answer": 3
+        },
+        {
+          "question": "According to the Supreme Court, the word forest must be understood according to its",
+          "options": ["biological meaning", "ecological meaning", "silvicultural meaning", "dictionary meaning"],
+          "answer": 3
+        },
+        {
+          "question": "Which of these is not a use value?",
+          "options": ["direct value", "indirect value", "option value", "existence value"],
+          "answer": 3
+        },
+        {
+          "question": "According to the Supreme Court, the term forest land includes",
+          "options": [
+            "some area recorded as forest in the Government record according to ownership",
+            "any area recorded as forest in the Government record according to ownership",
+            "some area recorded as forest in the Government record irrespective of ownership",
+            "any area recorded as forest in the Government record irrespective of ownership"
+          ],
+          "answer": 3
+        },
+        {
+          "question": "Tropical moist forests do not include",
+          "options": ["broadleaved forests", "wet evergreen forests", "semi-evergreen forests", "moist deciduous forests"],
+          "answer": 0
+        },
+        {
+          "question": "The value of leaving use and non-use values for offspring’s or future generations is called",
+          "options": ["altruistic value", "bequest value", "existence value", "option value"],
+          "answer": 1
+        },
+        {
+          "question": "The term 'forest' originates from",
+          "options": ["Latin foris meaning outside", "Greek foris meaning outside", "Latin foris meaning trees", "Greek foris meaning trees"],
+          "answer": 0
+        },
+        {
+          "question": "The value derived from the knowledge of use of resources by others in the current generation is called",
+          "options": ["altruistic value", "bequest value", "existence value", "option value"],
+          "answer": 0
+        }
+      ],
+      "week2": [
+      {
+        "question": "Lithosere is an example of",
+        "options": ["hydrosere", "xerosere", "psammosere", "halosere"],
+        "answer": 1
+      },
+      {
+        "question": "Which of these depicts correctly the lithosere primary succession?",
+        "options": [
+          "Rock → Crustose lichen → Foliose lichen → Moss → Herbaceous stage → Shrub → Woodland → Climax",
+          "Rock → Foliose lichen → Crustose lichen → Moss → Herbaceous stage → Shrub → Woodland → Climax",
+          "Moss → Crustose lichen → Foliose lichen → Rock → Herbaceous stage → Shrub → Woodland → Climax",
+          "Rock → Crustose lichen → Foliose lichen → Shrub → Herbaceous stage → Moss → Woodland → Climax"
+        ],
+        "answer": 0
+      },
+      {
+        "question": "At compensation point,",
+        "options": ["photosynthesis = respiration", "photosynthesis > respiration", "photosynthesis < respiration", "none of these"],
+        "answer": 0
+      },
+      {
+        "question": "The study of life history / general features of forest crops with respect to environmental factors is called",
+        "options": ["foresti-culture", "monoculture", "silviculture", "silvics"],
+        "answer": 3
+      },
+      {
+        "question": "A climax caused by wildfires is an example of",
+        "options": ["climatic climax", "edaphic climax", "disclimax", "catastrophic climax"],
+        "answer": 3
+      },
+      {
+        "question": "Net primary productivity is given by",
+        "options": ["APAR + LUE", "APAR X LUE", "APAR - LUE", "APAR ÷ LUE"],
+        "answer": 1
+      },
+      {
+        "question": "Practical application of scientific, technical and economic principles of forestry comes under which branch of forestry?",
+        "options": ["forest management", "forest economics", "forest mensuration", "forest protection"],
+        "answer": 0
+      },
+      {
+        "question": "In the context of plant nutrition, boron is",
+        "options": ["macronutrient", "micronutrient", "primary nutrient", "secondary nutrient"],
+        "answer": 1
+      },
+      {
+        "question": "Which of these is not a characteristic of pioneer species",
+        "options": ["ability to grow on bare rocks", "ability to tolerate extreme temperatures", "large size", "short life span"],
+        "answer": 2
+      },
+      {
+        "question": "The climax near Tindni village is being controlled by disturbance by cattle. This is an example of",
+        "options": ["climatic climax", "edaphic climax", "disclimax", "catastrophic climax"],
+        "answer": 2
+      }
+    ],
+    "week3": [
+      {
+        "question": "Mechanical action of ocean waves is an example of",
+        "options": ["chemical weathering", "physical weathering", "biological weathering", "none of the above"],
+        "answer": 1
+      },
+      {
+        "question": "Khadar is a type of",
+        "options": ["black cotton soil", "alluvial soil", "saline soil", "red and yellow soil"],
+        "answer": 1
+      },
+      {
+        "question": "In soil profile, C refers to",
+        "options": ["organic surface layer", "topsoil layer", "subsoil layer", "substratum layer"],
+        "answer": 3
+      },
+      {
+        "question": "Regur is a term for",
+        "options": ["black cotton soil", "alluvial soil", "saline soil", "red and yellow soil"],
+        "answer": 0
+      },
+      {
+        "question": "Bangar is a type of",
+        "options": ["black cotton soil", "alluvial soil", "saline soil", "red and yellow soil"],
+        "answer": 1
+      },
+      {
+        "question": "Soil formation is dependent upon",
+        "options": ["vegetation", "time", "climate", "all of the above"],
+        "answer": 3
+      },
+      {
+        "question": "Thermal stresses lead to",
+        "options": ["chemical weathering", "physical weathering", "biological weathering", "none of the above"],
+        "answer": 1
+      },
+      {
+        "question": "Carbonation is an example of",
+        "options": ["chemical weathering", "physical weathering", "biological weathering", "none of the above"],
+        "answer": 0
+      },
+      {
+        "question": "Cryofracturing is an example of",
+        "options": ["chemical weathering", "physical weathering", "biological weathering", "none of the above"],
+        "answer": 1
+      },
+      {
+        "question": "Hydrolysis is an example of",
+        "options": ["chemical weathering", "physical weathering", "biological weathering", "none of the above"],
+        "answer": 0
+      }
+    ],
+    "week4": [
+      {
+        "question": "For absolute form factor, the reference for the base of the cylinder is",
+        "options": ["base of the tree", "breast height", "10% of tree height", "20% of tree height"],
+        "answer": 0
+      },
+      {
+        "question": "For normal form factor, the reference for the base of the cylinder is",
+        "options": ["base of the tree", "breast height", "10% of tree height", "20% of tree height"],
+        "answer": 2
+      },
+      {
+        "question": "In India, the breast height is considered to be",
+        "options": ["1.37 m", "1.47 m", "1.57 m", "2.47 m"],
+        "answer": 0
+      },
+      {
+        "question": "Measurement of height based on similar triangles comes under:",
+        "options": ["similar measurement", "dissimilar measurement", "direct measurement", "indirect measurement"],
+        "answer": 3
+      },
+      {
+        "question": "For a tree with dbh = 45.6 cm, height = 27 m and total stem volume of 1.78 cum, the artificial form factor for the tree is:",
+        "options": ["0.1", "0.2", "0.3", "0.4"],
+        "answer": 3
+      },
+      {
+        "question": "Consider a stand of eucalyptus trees that are on average 30 cm in diameter and spaced on a regular 3 m grid. Find the spacing factor.",
+        "options": ["5", "10", "15", "data insufficient"],
+        "answer": 1
+      },
+      {
+        "question": "Diameter over bark (dob), diameter under bark (dub) and bark thickness (tb) are related as:",
+        "options": ["dob = dub + tb", "dob = dub - tb", "dob = dub + 2×tb", "dob = dub - 2 × tb"],
+        "answer": 2
+      },
+      {
+        "question": "For false form factor, the reference for the base of the cylinder is",
+        "options": ["base of the tree", "breast height", "10% of tree height", "20% of tree height"],
+        "answer": 1
+      },
+      {
+        "question": "Choose the correct statement:",
+        "options": [
+          "For a non-circular cross-section, girth tape over-estimates the sectional area.",
+          "For a non-circular cross-section, girth tape under-estimates the sectional area.",
+          "For a non-circular cross-section, girth tape correctly estimates the sectional area.",
+          "None of these is a correct statement."
+        ],
+        "answer": 0
+      },
+      {
+        "question": "A tree has dbh of 25 cm. Find its basal area in sq m.",
+        "options": [".049", ".096", ".149", ".195"],
+        "answer": 0
+      }
+    ],
+    "week5": [
+      {
+        "question": "“This sampling employs a simple rule of selecting every kth unit starting with a number chosen at random from 1 to k as the random start.” We're talking about",
+        "options": ["Simple random sampling", "Systematic sampling", "Stratified sampling", "Multistage sampling"],
+        "answer": 1
+      },
+      {
+        "question": "IMU stands for",
+        "options": ["Imperial metering unit", "Inertial metering unit", "Imperial measurement unit", "Inertial measurement unit"],
+        "answer": 3
+      },
+      {
+        "question": "___ is how close the measured values are to the correct value.",
+        "options": ["Accuracy", "Precision", "Bias", "Variance"],
+        "answer": 0
+      },
+      {
+        "question": "A list of sampling units is called a",
+        "options": ["frame", "window", "sample", "population"],
+        "answer": 0
+      },
+      {
+        "question": "The time of flight for LiDAR is 0.00001 sec. Find the distance of the object from the instrument.",
+        "options": ["500 m", "1000 m", "1500 m", "2000 m"],
+        "answer": 2
+      },
+      {
+        "question": "Which of these is correct?",
+        "options": [
+          "Plane surveying takes into account the true shape of the Earth and is used for smaller areas (< 250 sq km)",
+          "Plane surveying takes into account the true shape of the Earth and is used for larger areas (> 250 sq km)",
+          "Geodetic surveying takes into account the true shape of the Earth and is used for smaller areas (< 250 sq km)",
+          "Geodetic surveying takes into account the true shape of the Earth and is used for larger areas (> 250 sq km)"
+        ],
+        "answer": 3
+      },
+      {
+        "question": "A sampling procedure such that each possible combination of sampling units out of the population has the same chance of being selected is referred to as",
+        "options": ["Simple random sampling", "Systematic sampling", "Stratified sampling", "Multistage sampling"],
+        "answer": 0
+      },
+      {
+        "question": "The frequency of flyovers is an indicator of",
+        "options": ["spatial resolution", "temporal resolution", "spectral resolution", "radiometric resolution"],
+        "answer": 1
+      },
+      {
+        "question": "___ is how close the measured values are to each other.",
+        "options": ["Accuracy", "Precision", "Bias", "Variance"],
+        "answer": 1
+      },
+      {
+        "question": "Bathymetric LiDAR uses",
+        "options": ["far infrared light", "near infrared light", "orange light", "green light"],
+        "answer": 3
+      }
+    ]
+    }
+  }
+  
 // DOM elements
 const setupContainer = document.getElementById('setup-container');
 const quizContainer = document.getElementById('quiz-container');
